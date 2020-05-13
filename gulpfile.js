6f7994e859e576e2755cdb38379b4e8640bc3335
ankurnarkhede <ankurnarkhede@gmail.com>
2020-05-13 09:15:50 +0530

    Added loading state for in views

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,8 @@ sass.compiler = require('node-sass')
 const isEnvProduction = process.env.NODE_ENV === 'production'
 const paths = {
   dirs: {
-    dist: 'dist/'
+    dist: 'dist/',
+    scriptsSourcemaps: 'dist/javascripts/sourcemaps/'
   },
   handlebars: 'views/**/*.handlebars',
   images: 'public/**/*.{JPG,jpg,png,gif,ico}',
@@ -38,13 +39,14 @@ const clean = () => {
  */
 const scripts = () => {
   return gulp.src(paths.scripts)
-    .pipe(concat('app.js'))
     .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
     .pipe(babel({
       presets: ['@babel/env']
     }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulpif(isEnvProduction, uglify()))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.dirs.dist + '/javascripts'))
 }
 
@@ -54,11 +56,12 @@ const scripts = () => {
  */
 const styles = () => {
   return gulp.src(paths.styles)
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest(paths.dirs.dist))
+    .pipe(autoprefixer('last 2 version'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulpif(isEnvProduction, minifycss()))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.dirs.dist))
 }
 
